578f17b9a95cf410b72087cd4c898dd69eb87ac9
Lukas Frei <lukas.frei@zuehlke.com>
2021-09-18 13:53:13 +0200

    Format code

--- src/topology/topology-converter.js
+++ src/topology/topology-converter.js
@@ -10,10 +10,10 @@ function addSinkTopic(elements, processor, topic, xPos, yPos) {
     elements.push({
         id: topic,
         type: isRepartition(topic) ? 'default' : 'output',
-        data: {label: topic},
-        position: {x: xPos, y: yPos + 100},
+        data: { label: topic },
+        position: { x: xPos, y: yPos + 100 },
     })
-    elements.push({id: id(processor, topic), source: processor, target: topic, animated: true});
+    elements.push({ id: id(processor, topic), source: processor, target: topic, animated: true });
 }
 
 function addSourceTopic(elements, processor, topic, xPos, yPos) {
@@ -21,33 +21,33 @@ function addSourceTopic(elements, processor, topic, xPos, yPos) {
         elements.push({
             id: topic,
             type: 'input',
-            data: {label: topic},
-            position: {x: xPos, y: yPos},
+            data: { label: topic },
+            position: { x: xPos, y: yPos },
         });
     }
-    elements.push({id: id(topic, processor), source: topic, target: processor, animated: true});
+    elements.push({ id: id(topic, processor), source: topic, target: processor, animated: true });
 }
 
 function addStore(elements, processor, store, xPos, yPos) {
     elements.push({
         id: store,
         type: 'output',
-        data: {label: store},
-        position: {x: xPos + 200, y: yPos + 100},
+        data: { label: store },
+        position: { x: xPos + 200, y: yPos + 100 },
     })
 
     if (processor.includes("JOIN")) {
-        elements.push({id: id(store, processor), source: store, target: processor, animated: true});
+        elements.push({ id: id(store, processor), source: store, target: processor, animated: true });
     } else {
-        elements.push({id: id(processor, store), source: processor, target: store, animated: true});
+        elements.push({ id: id(processor, store), source: processor, target: store, animated: true });
     }
 }
 
 function addProcessor(elements, processor, xPos, yPos) {
     elements.push({
         id: processor,
-        data: {label: processor},
-        position: {x: xPos, y: yPos},
+        data: { label: processor },
+        position: { x: xPos, y: yPos },
     })
 }
 
@@ -56,62 +56,62 @@ function addStream(elements, processor, target) {
         return;
     }
 
-    elements.push({id: id(processor, target), source: processor, target: target, animated: true});
+    elements.push({ id: id(processor, target), source: processor, target: target, animated: true });
 }
 
 function convertTopologyToFlow(topology) {
     let elements = [];
-    
+
     let processor;
     let yPos = 0;
     let xPos = 0;
 
-	topology.split('\n').forEach(line => {
+    topology.split('\n').forEach(line => {
 
-		let processorMatch = /(Source:|Processor:|Sink:)\s+(\S+)\s+\((topics|topic|stores):(.*)\)/.exec(line)
+        let processorMatch = /(Source:|Processor:|Sink:)\s+(\S+)\s+\((topics|topic|stores):(.*)\)/.exec(line)
 
-		if (processorMatch) {
-			processor = processorMatch[2];
-			let type = processorMatch[3];
-			let states = processorMatch[4];
+        if (processorMatch) {
+            processor = processorMatch[2];
+            let type = processorMatch[3];
+            let states = processorMatch[4];
 
-			states.split(',').forEach(state => {
-				state = state.replace(/[[\]]/g, '').trim();
+            states.split(',').forEach(state => {
+                state = state.replace(/[[\]]/g, '').trim();
 
-				if (state === '') {
-					// short circuit
-				}
+                if (state === '') {
+                    // short circuit
+                }
                 else if (type === 'topic') {
                     addSinkTopic(elements, processor, state, xPos, yPos);
                 }
-				else if (type === 'topics') {
+                else if (type === 'topics') {
                     addSourceTopic(elements, processor, state, xPos, yPos);
                     yPos = yPos + 100;
-				}
-				else if (type === 'stores') {
+                }
+                else if (type === 'stores') {
                     addStore(elements, processor, state, xPos, yPos);
                 }
-			});
+            });
 
             addProcessor(elements, processor, xPos, yPos);
             yPos = yPos + 100;
 
-			return;
-		}
+            return;
+        }
 
-		let streamMatch = /-->\s+(.*)$/.exec(line.trim());
+        let streamMatch = /-->\s+(.*)$/.exec(line.trim());
 
-		if (streamMatch && processor) {
-			let targets = streamMatch[1];
+        if (streamMatch && processor) {
+            let targets = streamMatch[1];
 
-			targets.split(',').forEach(target => {
-				target = target.trim();
+            targets.split(',').forEach(target => {
+                target = target.trim();
                 addStream(elements, processor, target);
             });
-		}
-	})
+        }
+    })
 
-	return elements;
+    return elements;
 }
 
 export default convertTopologyToFlow;
