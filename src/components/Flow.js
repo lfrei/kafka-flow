5d3147b613e84eb2b8f3610a39f8e543f4cdc41c
Lukas Frei <lukas.frei@zuehlke.com>
2021-09-18 14:40:50 +0200

    Add input to update rendered topology

--- src/components/Flow.js
+++ src/components/Flow.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import './Flow.css';
+import React, { useEffect, useState } from 'react';
 import ReactFlow from 'react-flow-renderer';
 import topology from '../topology/topology-example.json';
 import convertTopologyToFlow from '../topology/topology-converter.js';
 
 function Flow() {
-    const elements = convertTopologyToFlow(topology.simple);
+    const initialTopology = topology.complex;
+    const initialElements = convertTopologyToFlow(initialTopology);
+
+    const [elements, setElements] = useState(initialElements);
+    const [newTopology, setTopology] = useState(initialTopology);
+
+    useEffect(() => setElements(() => convertTopologyToFlow(newTopology)), [newTopology, setElements]);
 
     return (
         <div style={{ height: 1200 }}>
+            <div className="update-topology-control">
+                <textarea
+                    rows="20"
+                    cols="50"
+                    value={newTopology}
+                    onChange={(evt) => setTopology(evt.target.value)} />
+            </div>
             <ReactFlow elements={elements} />
         </div>
     )
