4156a9ad5fd2f6e147864bcde9fd4243c999d9ed
Lukas Frei <lukas.frei@zuehlke.com>
2021-09-20 18:27:18 +0200

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import Flow from './components/Flow.js'
 import Header from './components/Header.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="container-fluid">
-      <Header />
-      <Flow />
-    </div>
+    <Router>
+      <div className="container-fluid">
+        <Header />
+        <Switch>
+          <Route path="/">
+            <Flow />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
