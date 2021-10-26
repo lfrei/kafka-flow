c09a14b452ad2ef321c032c0c65a12e2075e1c58
Lukas Frei <lukas.frei@zuehlke.com>
2021-10-26 13:56:41 +0200

    Show tooltip for abbreviated names of nodes

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,14 +1,33 @@
 import './Node.css';
+import ReactTooltip from 'react-tooltip';
 
 function Node({ type, name, offset }) {
+    const showTooltip = name.length > 26;
+    const showOffset = offset >= 0;
+
     return (
-        <div className={type}>
-            {name}
-            {offset >= 0 &&
-                <span>:
-                    <div className="offset">{offset}</div>
-                </span>
+        <div>
+            {showTooltip &&
+                <ReactTooltip id={name}
+                    place="bottom"
+                    effect="solid"
+                    type="light"
+                    border="true"
+                    borderColor="lightgrey"
+                    offset={{ bottom: 10 }}>
+
+                    <p>{name}</p>
+                </ReactTooltip>
             }
+
+            <div className={type} data-tip data-for={name}>
+                {name}
+                {showOffset &&
+                    <span>:
+                        <div className="offset">{offset}</div>
+                    </span>
+                }
+            </div>
         </div>
     )
 }
